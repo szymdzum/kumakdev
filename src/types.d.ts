f365285c4dc1b55c745f639383aa6c3d7eb4eaf8
Kumak <szymon@kumak.dev>
2025-03-25 05:35:05 +0100

    be sharp

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,29 @@ declare module 'clsx' {
 declare module 'tailwind-merge' {
   export function twMerge(...inputs: string[]): string;
 }
+
+declare namespace astroHTML.JSX {
+  interface HTMLAttributes {
+    'transition:persist'?: boolean;
+    'transition:animate'?: string;
+    'transition:name'?: string;
+  }
+}
+
+declare module 'astro:transitions' {
+  interface ViewTransitionsOptions {
+    fallback?: 'animate' | 'swap' | 'none';
+    persist?: string[];
+    handleForms?: boolean;
+  }
+
+  interface ViewTransitionsProps extends ViewTransitionsOptions {
+    [key: string]: unknown;
+  }
+
+  interface ViewTransitionsComponent {
+    new (props?: ViewTransitionsProps): Element;
+  }
+
+  export const ViewTransitions: ViewTransitionsComponent;
+}
