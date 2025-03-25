cf27b1efd6ccd5512943853743a38064d1164998
Kumak <szymon@kumak.dev>
2025-03-25 03:21:24 +0100

    Update changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,34 +1,30 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "kumak does dev",
+  DESCRIPTION: "kumak does dev",
+  EMAIL: "szymon@kumak.dev",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "kumak does dev",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
+  DESCRIPTION: "kumak writes about dev",
+}
 
-export const PROJECTS: Metadata = {
+ export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "kumak robi paczki",
 };
 
 export const SOCIALS: Socials = [
-  {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
-  },
   {
     NAME: "GitHub",
     HREF: "https://github.com/trevortylerlee",
